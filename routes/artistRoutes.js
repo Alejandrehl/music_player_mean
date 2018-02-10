864604f0323056992cd125313129c000413f6d09
Alejandro Hernández <Alejandrehl@icloud.com>
2018-02-10 18:52:43 -0300

    save artist, get artist, get artists & pagination

--- routes/artistRoutes.js
+++ routes/artistRoutes.js
@@ -5,6 +5,8 @@ var md_auth = require('../middlewares/authenticated');
 
 var ArtistController = require('../controllers/artistController');
 
-api.get('/artist', md_auth.ensureAuth, ArtistController.getArtist);
+api.get('/artist/:id', md_auth.ensureAuth, ArtistController.getArtist);
+api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
+api.get('/artists/:page?', md_auth.ensureAuth, ArtistController.getArtists);
 
 module.exports = api;

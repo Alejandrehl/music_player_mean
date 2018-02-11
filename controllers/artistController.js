ab8ee3bdd1ce120887f47d5d091f4caaff7387b0
Alejandro Hernández <Alejandrehl@icloud.com>
2018-02-10 21:21:26 -0300

    Artist done

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -83,8 +83,150 @@ function saveArtist(req, res){
 	});
 }
 
+function updateArtist(req, res){
+	var artistId = req.params.id;
+	var update = req.body;
+
+	Artist.findByIdAndUpdate(artistId, update, (err, artistUpdated) => {
+		if(err){
+			res.status(500).send({
+				message: 'Error al guardar el artista'
+			});
+		}else{
+			if(!artistUpdated){
+				res.status(404).send({
+					message: 'El artista no ha sido actualizado'
+				});
+			}else{
+				res.status(200).send({
+					artist: artistUpdated
+				});
+			}
+		}	
+	});
+}
+
+function deleteArtist(req, res){
+	var artistId = req.params.id;
+
+	Artist.findByIdAndRemove(artistId, (err, artistRemoved) => {
+		if(err){
+			res.status(500).send({
+				message: 'Error al eliminar el artista'
+			});
+		}else{
+			if(!artistRemoved){
+				res.status(404).send({
+					message: 'El artista no ha sido eliminado'
+				});
+			}else{
+				Album.find({
+					artist: artistRemoved._id
+				}).remove((err, albumRemoved) => {
+					if(err){
+						res.status(500).send({
+							message: 'Error al eliminar el album'
+						});
+					}else{
+						if(!albumRemoved){
+							res.status(404).send({
+								message: 'El album no ha sido eliminado'
+							});
+						}else{
+							Song.find({
+								album: albumRemoved._id
+							}).remove((err, songRemoved) => {
+								if(err){
+									res.status(500).send({
+										message: 'Error al intentar eliminar la canción'
+									});
+								}else{
+									if(!songRemoved){
+										res.status(404).send({
+											message: 'La canción no ha sido eliminada'
+										});
+									}else{
+										res.status(200).send({
+											artist: artistRemoved
+										});
+									}
+								}
+							});
+						}
+				}});
+			}
+		}
+	});
+}
+
+//Subir imagen
+function uploadImage(req, res){
+	var artistId = req.params.id;
+	var file_name = 'No subido...';
+
+	//Con el connect-multiparty (declarada en artistRoutes)
+	//Podemos utilizar las variables globales como files
+	if(req.files){
+		var file_path = req.files.image.path;
+		var file_split = file_path.split('/');
+		var file_name = file_split[2];
+
+		//Sacar extensión de imagen
+		var ext_split = file_name.split('\.');
+		var file_ext = ext_split[1];
+
+		if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif'){
+
+			Artist.findByIdAndUpdate(artistId, {image: file_name}, (err, artistUpdated) => {
+				if(!artistUpdated){
+					res.status(400).send({
+						message: 'Extensión del archivo no valida'
+					});
+				}else{
+					res.status(200).send({
+						user: artistUpdated
+					});	
+				}
+			});
+
+		}else{
+			res.status(200).send({
+				message: 'Extensión del archivo no valido'
+			});
+		}
+
+		console.log(file_path);
+		console.log(file_split);
+		console.log(file_name);
+	}else{
+		res.status(200).send({
+			message: 'No has subido ninguna imagen ...'
+		});		
+	}
+}
+
+//Obtener imagen de usuario
+function getImageFile(req, res){
+	var imageFile = req.params.imageFile;
+	var path_file = './uploads/artists/'+imageFile;
+
+	fs.exists(path_file, function(exists){
+		if(exists){
+			res.status(200).sendFile(path.resolve(path_file));	
+		}else{
+			res.status(200).send({
+				message: 'No existe la imagen ...'
+			});	
+		}
+	});
+}
+
 module.exports = {
 	getArtist,
 	saveArtist,
-	getArtists
+	getArtists,
+	updateArtist,
+	deleteArtist,
+	uploadImage,
+	getImageFile
 };

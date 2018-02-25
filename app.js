e49771cf8adfd1ad9f01a64b2d8de2e9fa922e7b
Alejandro Hernández <Alejandrehl@icloud.com>
2018-02-25 13:59:29 -0300

    Configuración de cabeceras Http ok, obtener el nombre de la imagen del usuario ok, backend done

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 //Configurar cabeceras https
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+	next();
+});
 
 //Cargar rutas base
 app.use('/api', user_routes); //incluir middleware /api
